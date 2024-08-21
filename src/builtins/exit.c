af7a1d0728c66573d3f7b8c93a03fdde1e6491cd
Xander Azuaje <charon-mortis@pm.me>
2024-08-21 19:13:08 +0200

    exit fix
    # Conflicts:
    #	src/builtins/exit.c

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -2,23 +2,21 @@
 
 void do_exit(char **args)
 {
-    //TENGO QUE PRINTEAR PREV_EXIT_STATUS PARA VER CÓMO FUNCIONA Y VER SI LO MODIFICO AHORA O LUEGO
-    int exit_status = prev_exit_status(0);  //coge el último estado sin modificarlo al principio
+    int exit_status = prev_exit_status(0);
 
     if (args[1] != NULL)
     {
         int status;
         status = ft_atoi(args[1]);
         
-        if (status >= 0 && status <= 255)
-            exit_status = status;
+        if (status < 0)
+          exit_status = 244;
+        else if (status > 255)
+            exit_status = 179;
         else
-        {
-            printf("errorcito\n");
-            exit_status = 2;
-        }
+            exit_status = status;
     }
-    // prev_exit_status(exit_status);????? no lo sé
+    prev_exit_status(exit_status);
     printf("byebye<3\n");
     exit(exit_status);
     //intuyo que después de ejecutar el comando habrá que pasarle el status nuevo a prev_Exit_Status
