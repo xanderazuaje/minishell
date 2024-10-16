2f50c312031b9c93f2e1ab0d7f9c2ec366df72bd
Xander Azuaje <charon-mortis@pm.me>
2024-10-16 10:35:04 +0200

    removed comments and debug info

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xazuaje- <xazuaje-@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/30 02:48:27 by xazuaje-          #+#    #+#             */
+/*   Updated: 2024/10/16 10:34:14 by xazuaje-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "builtins.h"
 
 int add_to_env(char *new_var, char **env, int len)
@@ -45,8 +57,6 @@ int check_env(char *name, char *value, char **new_env, char **env)
 
     i = 0;
     namelen = ft_strlen(name);
-    printf("esto mide name: %d\n", namelen);
-    printf("esto mide value: %zu\n", ft_strlen(value));
     new_var = malloc((namelen + ft_strlen(value)) * sizeof(char *)); //crear nueva variable name=value
     if (!new_var)
         return -1;
@@ -63,8 +73,6 @@ int check_env(char *name, char *value, char **new_env, char **env)
         j++;
     }
     new_var[i] = '\0';
-    printf("esto mide la nueva variable: %zu\n", namelen + ft_strlen(value));
-    printf("esto es new_var: %s\n", new_var);
     i = 0;
     while (new_env[i] != NULL) //mirar si existe ya la variable
     {
@@ -76,8 +84,6 @@ int check_env(char *name, char *value, char **new_env, char **env)
         }
         i++;
     }
-    printf("aquí falla?¿?¿?¿?¿\n");
-    //si no existe la agrego
     if (add_to_env(new_var, env, i) == -1)
         return (perror("export"), 1);
     return 0;
@@ -85,20 +91,18 @@ int check_env(char *name, char *value, char **new_env, char **env)
 
 int builtin_export(char *str, char **env) 
 {
-    char *equal_sign;
-    char *name; //export NAME=VALUE
-    char **new_env;
-    int i;
+    char	*equal_sign;
+    char	*name;
+    char	**new_env;
+    int	    i;
 
     i = 0;
-    equal_sign = ft_strchr(str, '='); //busca a ver donde está el = (a partir de ahí, se coloca)
+    equal_sign = ft_strchr(str, '=');
     if (!equal_sign) 
     {
         perror("export: not a valid identifier");
         return 1;
     }
-    printf("equal sign: %s\n", equal_sign); //AQUI SACO =VALUE O LO QUE VA DETRÁS DEL NAME
-    printf("esto ocupa name: %zu\n", len_to_char(str, '=') - 1);
     name = malloc(len_to_char(str, '=') * sizeof(char *)); //incluye la longitud de name + 1
     if (!name)
         return -1;
@@ -108,7 +112,6 @@ int builtin_export(char *str, char **env)
         i++;
     }
     name[i] = '\0';
-    printf("esto mide env: %d\n", envlen(env));
     new_env = malloc((envlen(env) + 1) * sizeof(char *));
     if (!new_env)
          return -1;
@@ -120,7 +123,7 @@ int builtin_export(char *str, char **env)
     }
     new_env[i] = NULL;
     if (check_env(name, equal_sign, new_env, env) != 0) 
-        return (perror("export2"), 1);
+        return (perror("export"), 1);
     return 0;
 }
 
--- src/parsing/expansor/get_var_val.c
+++ src/parsing/expansor/get_var_val.c
@@ -3,6 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   get_var_val.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
+/*   By: xazuaje- <xazuaje-@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/04 20:11:28 by xazuaje-          #+#    #+#             */
+/*   Updated: 2024/10/16 10:33:03 by xazuaje-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_var_val.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
 /*   By: mhiguera <mhiguera@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 00:49:53 by xazuaje-          #+#    #+#             */
@@ -21,12 +33,10 @@ char	*get_var_val(const char *str, char **env, int exit_status)
 	if (*(str + 1) == '?')
 		return (ft_itoa(exit_status));
 	var = NULL;
-	printf("*env: %s\n", *env);
 	while (*env && !start_with(*env, str + 1))
 		env++;
 	if (!*env)
 		return (NULL);
-	printf("hola llego aquí acaso?\n");
 	var = *env;
 	while (*var != '=')
 	{
