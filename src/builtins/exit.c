c07befe43e9134bfdfc44b81ea191ab829dfb60e
Xander Azuaje <charon-mortis@pm.me>
2024-11-08 14:27:51 +0100

    norm

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: xazuaje- <xazuaje-@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 13:33:47 by xazuaje-          #+#    #+#             */
-/*   Updated: 2024/11/08 14:25:19 by xazuaje-         ###   ########.fr       */
+/*   Updated: 2024/11/08 14:27:43 by xazuaje-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,29 +31,35 @@ void	exit_error(char *str)
 	write(2, ": numeric argument required\n", 28);
 }
 
-void do_exit(char **args, char ***env)
+void	check_valid_input(char **args, int *exit_status, int i)
 {
-	int exit_status;
-	int i;
+	if (args[1][i] != '\0')
+	{
+		exit_error(args[1]);
+		*exit_status = 2;
+	}
+	if (args[2])
+	{
+		write(2, "exit: too many arguments\n", 26);
+		*exit_status = 1;
+	}
+	else
+		*exit_status = ft_atoi(args[1]) % 256;
+}
+
+void	do_exit(char **args, char ***env)
+{
+	int	exit_status;
+	int	i;
 
 	i = 0;
 	exit_status = prev_exit_status(0);
 	if (args[1])
 	{
-		while (args[1][i] && (ft_isdigit(args[1][i]) || (i == 0 && args[1][i] == '-')))
+		while (args[1][i] && (ft_isdigit(args[1][i])
+			|| (i == 0 && args[1][i] == '-')))
 			i++;
-
-		if (args[1][i] != '\0')
-		{
-			exit_error(args[1]);
-			exit_status = 2;
-		}
-		if (args[2])
-		{
-			write(2, "exit: too many arguments\n", 26);
-			exit_status = 1;
-		} else
-			exit_status = ft_atoi(args[1]) % 256;
+		check_valid_input(args, &exit_status, i);
 	}
 	freeenv(env);
 	prev_exit_status(exit_status);
