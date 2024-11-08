5839357f5525416b24d49228cbf592efc91d02bb
Xander Azuaje <charon-mortis@pm.me>
2024-11-08 12:41:50 +0100

    norminette

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -3,6 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
+/*   By: xazuaje- <xazuaje-@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/08 12:25:43 by xazuaje-          #+#    #+#             */
+/*   Updated: 2024/11/08 12:40:32 by xazuaje-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
 /*   By: mhiguera <mhiguera@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 02:48:27 by xazuaje-          #+#    #+#             */
@@ -12,7 +24,6 @@
 
 #include "builtins.h"
 
-//falla que tiene que tener un = si o si para poder exportarse
 void	export_error(char *str)
 {
 	write(2, "export: ", 8);
@@ -32,8 +43,8 @@ int	builtin_export(char *str, char ***env)
 	}
 	while (str[i] && str[i] != '=')
 	{
-		if ((!ft_isalnum(str[i]) && str[i] != '_') 
-		|| (!ft_isalpha(str[0]) && str[0] != '_'))
+		if ((!ft_isalnum(str[i]) && str[i] != '_')
+			|| (!ft_isalpha(str[0]) && str[0] != '_'))
 		{
 			export_error(str);
 			return (1);
@@ -41,7 +52,7 @@ int	builtin_export(char *str, char ***env)
 		i++;
 	}
 	if (str[i] != '=')
-		return (0); //falta arreglar echo $?
+		return (0);
 	insert_variable(str, env);
 	return (0);
 }
