b65f9d8992017c035035f7659934ac2a2419f780
Xander Azuaje <charon-mortis@pm.me>
2024-11-13 20:14:34 +0100

    corrected error code
    # Conflicts:
    #	src/globals/globals.h
    #	src/globals/prev_exit_status.c

--- src/globals/globals.h
+++ src/globals/globals.h
@@ -6,12 +6,15 @@
 /*   By: xazuaje- <xazuaje-@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 00:46:03 by xazuaje-          #+#    #+#             */
-/*   Updated: 2024/11/13 20:02:01 by xazuaje-         ###   ########.fr       */
+/*   Updated: 2024/06/27 17:53:35 by xazuaje-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GLOBALS_H
 # define GLOBALS_H
+# include <signal.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 extern int		g_exit_status;
 void			set_exit_status(int i);
--- src/globals/prev_exit_status.c
+++ src/globals/prev_exit_status.c
@@ -6,15 +6,25 @@
 /*   By: xazuaje- <xazuaje-@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 00:45:30 by xazuaje-          #+#    #+#             */
-/*   Updated: 2024/11/13 19:33:06 by xazuaje-         ###   ########.fr       */
+/*   Updated: 2024/11/15 23:34:18 by xazuaje-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 
 #include "globals.h"
 
-void	set_exit_status(int i)
+int	prev_exit_status(int i)
 {
-	g_exit_status = WEXITSTATUS(i);
+	if (WIFSIGNALED(i))
+	{
+		g_exit_status = 128 + WTERMSIG(i);
+		if (WTERMSIG(i) == SIGQUIT)
+			write(2, "Core dump\n", 11);
+		else if (WTERMSIG(i) == SIGINT)
+			write(2, "\n", 1);
+	}
+	else if (WIFEXITED(i))
+		g_exit_status = WEXITSTATUS(i);
+	else
+		g_exit_status = EXIT_FAILURE;
 }
